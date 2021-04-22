3dfae146b70ee7450a64acf51cfe973dceacc725
cartonni <nicolas.carton@natixis.com>
2021-04-22 16:25:08 +0200

    commit

--- Thanos.java
+++ Thanos.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Thanos {
@@ -12,14 +14,21 @@ public class Thanos {
         heroes.add(new Hero("Thor", 1501));
 
         // TODO 1 : Modify Hero to implements Comparable and sort by name (ascending)
-
-
+        Collections.sort(heroes);
+        
         System.out.println("\nOrder by name:");
         showList(heroes);
 
         // TODO 2: Add a Comparator and sort by age (descending)
-
-
+        Comparator<Hero> compare = new Comparator<Hero>() {
+            @Override
+            public int compare(Hero hero1, Hero hero2) {
+                return hero1.getAge() - hero2.getAge();
+            }
+            
+        };
+        Collections.sort(heroes, compare);
+        
         System.out.println("\nOrder by age:");
         showList(heroes);
     }
